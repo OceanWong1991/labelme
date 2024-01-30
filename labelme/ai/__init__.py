936e8095ceaca97ad5b06587cf65b4da08dae3d4
xzwang <xz.wang@invo.cn>
2024-01-30 11:05:55 +0800

    Update AI init

--- labelme/ai/__init__.py
+++ labelme/ai/__init__.py
@@ -2,21 +2,31 @@ import gdown
 
 from .efficient_sam import EfficientSam
 from .segment_anything_model import SegmentAnythingModel
-
+from pathlib import Path
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[2]  # labelme root directory
+# print('========FILE ==========', FILE)
+# print('========ROOT ==========', ROOT)
+# print('========FILE.parents[0] ==========', FILE.parents[0])
+# print('========FILE.parents[1] ==========', FILE.parents[1])
+# print('========FILE.parents[2] ==========', FILE.parents[2])
+# print('========FILE.parents[3] ==========', FILE.parents[3])
 
 class SegmentAnythingModelVitB(SegmentAnythingModel):
     name = "SegmentAnything (speed)"
 
     def __init__(self):
         super().__init__(
-            encoder_path=gdown.cached_download(
-                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_b_01ec64.quantized.encoder.onnx",  # NOQA
-                md5="80fd8d0ab6c6ae8cb7b3bd5f368a752c",
-            ),
-            decoder_path=gdown.cached_download(
-                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_b_01ec64.quantized.decoder.onnx",  # NOQA
-                md5="4253558be238c15fc265a7a876aaec82",
-            ),
+             encoder_path= ROOT / 'weights/sam_speed.quantized.encoder.onnx',
+             decoder_path= ROOT / 'weights/sam_speed.quantized.decoder.onnx',
+            # encoder_path=gdown.cached_download(
+            #     url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_b_01ec64.quantized.encoder.onnx",  # NOQA
+            #     md5="80fd8d0ab6c6ae8cb7b3bd5f368a752c",
+            # ),
+            # decoder_path=gdown.cached_download(
+            #     url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_b_01ec64.quantized.decoder.onnx",  # NOQA
+            #     md5="4253558be238c15fc265a7a876aaec82",
+            # ),
         )
 
 
@@ -25,14 +35,16 @@ class SegmentAnythingModelVitL(SegmentAnythingModel):
 
     def __init__(self):
         super().__init__(
-            encoder_path=gdown.cached_download(
-                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_l_0b3195.quantized.encoder.onnx",  # NOQA
-                md5="080004dc9992724d360a49399d1ee24b",
-            ),
-            decoder_path=gdown.cached_download(
-                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_l_0b3195.quantized.decoder.onnx",  # NOQA
-                md5="851b7faac91e8e23940ee1294231d5c7",
-            ),
+            encoder_path= ROOT / 'weights/sam_balanced.quantized.encoder.onnx',
+            decoder_path= ROOT / 'weights/sam_balanced.quantized.decoder.onnx',
+            # encoder_path=gdown.cached_download(
+            #     url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_l_0b3195.quantized.encoder.onnx",  # NOQA
+            #     md5="080004dc9992724d360a49399d1ee24b",
+            # ),
+            # decoder_path=gdown.cached_download(
+            #     url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_l_0b3195.quantized.decoder.onnx",  # NOQA
+            #     md5="851b7faac91e8e23940ee1294231d5c7",
+            # ),
         )
 
 
@@ -41,14 +53,16 @@ class SegmentAnythingModelVitH(SegmentAnythingModel):
 
     def __init__(self):
         super().__init__(
-            encoder_path=gdown.cached_download(
-                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_h_4b8939.quantized.encoder.onnx",  # NOQA
-                md5="958b5710d25b198d765fb6b94798f49e",
-            ),
-            decoder_path=gdown.cached_download(
-                url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_h_4b8939.quantized.decoder.onnx",  # NOQA
-                md5="a997a408347aa081b17a3ffff9f42a80",
-            ),
+            encoder_path= ROOT / 'weights/sam_accuracy.quantized.encoder.onnx',
+            decoder_path= ROOT / 'weights/sam_accuracy.quantized.decoder.onnx',
+            # encoder_path=gdown.cached_download(
+            #     url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_h_4b8939.quantized.encoder.onnx",  # NOQA
+            #     md5="958b5710d25b198d765fb6b94798f49e",
+            # ),
+            # decoder_path=gdown.cached_download(
+            #     url="https://github.com/wkentaro/labelme/releases/download/sam-20230416/sam_vit_h_4b8939.quantized.decoder.onnx",  # NOQA
+            #     md5="a997a408347aa081b17a3ffff9f42a80",
+            # ),
         )
 
 
@@ -57,14 +71,16 @@ class EfficientSamVitT(EfficientSam):
 
     def __init__(self):
         super().__init__(
-            encoder_path=gdown.cached_download(
-                url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vitt_encoder.onnx",  # NOQA
-                md5="2d4a1303ff0e19fe4a8b8ede69c2f5c7",
-            ),
-            decoder_path=gdown.cached_download(
-                url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vitt_decoder.onnx",  # NOQA
-                md5="be3575ca4ed9b35821ac30991ab01843",
-            ),
+            encoder_path= ROOT / 'weights/efficient_sam_speed_encoder.onnx',
+            decoder_path= ROOT / 'weights/efficient_sam_speed_decoder.onnx',
+            # encoder_path=gdown.cached_download(
+            #     url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vitt_encoder.onnx",  # NOQA
+            #     md5="2d4a1303ff0e19fe4a8b8ede69c2f5c7",
+            # ),
+            # decoder_path=gdown.cached_download(
+            #     url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vitt_decoder.onnx",  # NOQA
+            #     md5="be3575ca4ed9b35821ac30991ab01843",
+            # ),
         )
 
 
@@ -73,14 +89,16 @@ class EfficientSamVitS(EfficientSam):
 
     def __init__(self):
         super().__init__(
-            encoder_path=gdown.cached_download(
-                url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vits_encoder.onnx",  # NOQA
-                md5="7d97d23e8e0847d4475ca7c9f80da96d",
-            ),
-            decoder_path=gdown.cached_download(
-                url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vits_decoder.onnx",  # NOQA
-                md5="d9372f4a7bbb1a01d236b0508300b994",
-            ),
+            encoder_path= ROOT / 'weights/efficient_sam_accuracy_encoder.onnx',
+            decoder_path= ROOT / 'weights/efficient_sam_accuracy_decoder.onnx',
+            # encoder_path=gdown.cached_download(
+            #     url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vits_encoder.onnx",  # NOQA
+            #     md5="7d97d23e8e0847d4475ca7c9f80da96d",
+            # ),
+            # decoder_path=gdown.cached_download(
+            #     url="https://github.com/labelmeai/efficient-sam/releases/download/onnx-models-20231225/efficient_sam_vits_decoder.onnx",  # NOQA
+            #     md5="d9372f4a7bbb1a01d236b0508300b994",
+            # ),
         )
 
 
